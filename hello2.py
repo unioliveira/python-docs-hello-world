3ab05eb7f753a68e94cfa547eb51539e39c0d18e
unioliveira <71647799+unioliveira@users.noreply.github.com>
2024-02-01 20:16:44 -0300

    Update hello2.py

--- hello2.py
+++ hello2.py
@@ -1,8 +1,8 @@
 import sys
-print ('Executing script file is:', str(sys.argv[0]))
-print ('The arguments are:', str(sys.argv))
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("--world", help="Provide the name of the world to greet.")
-args = parser.parse_args()
-print ('Hello ', args.world)
+#print ('Executing script file is:', str(sys.argv[0]))
+#print ('The arguments are:', str(sys.argv))
+#import argparse
+#parser = argparse.ArgumentParser()
+#parser.add_argument("--world", help="Provide the name of the world to greet.")
+#args = parser.parse_args()
+print ('Hello World')
