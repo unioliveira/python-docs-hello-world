4097d6823384f743936a02b1375530d007cf9fd6
unioliveira <71647799+unioliveira@users.noreply.github.com>
2024-02-01 20:54:37 -0300

    Create test_app2.py

---
+++ test_app2.py
@@ -0,0 +1,8 @@
+import sys
+#print ('Executing script file is:', str(sys.argv[0]))
+#print ('The arguments are:', str(sys.argv))
+#import argparse
+#parser = argparse.ArgumentParser()
+#parser.add_argument("--world", help="Provide the name of the world to greet.")
+#args = parser.parse_args()
+print ('Hello World - Teste')
